981c06bbc11d6f32ebaea318dd86f9ca26055b20
Daniel Fleischman <danielfleischman@gmail.com>
2013-10-01 00:01:01 -0400

    Changed the javascript coding style
    Changed to the "sieve" style, where we first sieve out invalid inputs
    and then assume that the input is correct.

--- slide.js
+++ slide.js
@@ -2,31 +2,25 @@ var slides = null;
 var slide_ndx = 0;
 
 function init() {
-    slides = d3.selectAll(".slide");
-    slide_ndx = 0;
+	slides = d3.selectAll(".slide");
+	slide_ndx = 0;
 
-    slides.style("display", "none");
-    d3.select(".slide").style("display", "block");
+	slides.style("display", "none");
+	d3.select(".slide").style("display", "block");
 }
 
 function choose_current(d, i) {
-    if (i == slide_ndx) {
-    	d3.select(this).style("display", "block");
-    } else {
-    	d3.select(this).style("display", "none");
-    }
+	d3.select(this).style("display", (i == slide_ndx)?"block":"none");
 }
 
 function previous() {
-    if (slide_ndx > 0) {
+	if (slide_ndx == 0) return;	
 	slide_ndx = slide_ndx - 1;
 	slides.each(choose_current);
-    }
 }
 
 function next() {
-    if (slide_ndx < slides.size() - 1) {
+	if (slide_ndx == slides.size() - 1) return;
 	slide_ndx = slide_ndx + 1;
 	slides.each(choose_current);
-    }
 }
