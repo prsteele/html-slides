891ff5a392372aab2dc09f47eec3280c4ac0f2fa
Patrick Steele <prs233@cornell.edu>
2013-10-01 14:33:47 -0400

    Simplified the logic by taking advantage of chained selectors.

--- slide.js
+++ slide.js
@@ -12,7 +12,7 @@ var current_slide = null;
 
 // The D3 selection containing the elements to be revealed on the
 // current slide
-var current_reveals = null;
+var reveals = null;
 
 // Are we moving forwards or backwards in the slide deck? Needed when
 // computing which items to reveal when transitioning between slides.
@@ -38,29 +38,21 @@ function choose_current(d, i) {
 }
 
 function choose_reveal(d, i) {
-    var sel = d3.select(this);
-    sel.classed("revealed", i <= reveal_ndx)
-    sel.classed("unrevealed", i > reveal_ndx)
-}
-
-function has_next_reveal(selection) {
-    return reveal_ndx < selection.selectAll(".reveal").size() - 1;
-}
-
-function has_prev_reveal(selection) {
-    return reveal_ndx > -1;
+    d3.select(this)
+	.classed("revealed", i <= reveal_ndx)
+	.classed("unrevealed", i > reveal_ndx);
 }
 
 function previous() {
     forwards = false;
 
-    if (has_prev_reveal(current_slide)) {
+    if (reveals != null && reveal_ndx > -1) {
 	// If the current slide can un-reveal something, do so
 	reveal_ndx -= 1;
-	current_slide.selectAll(".reveal").each(choose_reveal);
+	reveals.each(choose_reveal);
     } else if (slide_ndx > 0) {
 	// Otherwise, try to go back one slide
-	slide_ndx = slide_ndx - 1;
+	slide_ndx -= 1;
 	slides.each(choose_current);
     }
 }
@@ -68,10 +60,10 @@ function previous() {
 function next() {
     forwards = true;
 
-    if (has_next_reveal(current_slide)) {
+    if (reveals != null && reveal_ndx < reveals.size() - 1) {
 	// If the current slide can reveal something, do so
 	reveal_ndx += 1;
-	current_slide.selectAll(".reveal").each(choose_reveal);
+	reveals.each(choose_reveal);
     } else if (slide_ndx < slides.size() - 1) {
 	// Otherwise, try to go forward one slide
 	slide_ndx = slide_ndx + 1;
